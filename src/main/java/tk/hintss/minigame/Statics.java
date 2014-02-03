838a924d4a5e3073e7fc9ab89d8e6ea73b905f38
Henry Ammermann <hintsss@gmail.com>
2014-02-03 03:02:58 -0800

    more useless comments \./

--- src/main/java/tk/hintss/minigame/Statics.java
+++ src/main/java/tk/hintss/minigame/Statics.java
@@ -9,22 +9,32 @@ public class Statics {
     }
 
     public static String getPlayerJoinMessage(Player p) {
+        // when a player joins the game
+
         return ChatColor.AQUA + p.getName() + ChatColor.GREEN + " joined the game!";
     }
 
     public static String getPlayerQuitMessage(Player p) {
+        // when a player leaves the game (voluntarily)
+
         return ChatColor.AQUA + p.getName() + ChatColor.GREEN + " left the game!";
     }
 
     public static String getPlayerWinMessage(Player p) {
+        // when someone wins
+
         return ChatColor.AQUA + p.getName() + ChatColor.GREEN + " won the game!";
     }
 
     public static String getReloadMessage() {
+        // sent to the players when the plugin is reloaded
+
         return ChatColor.RED + "The game was ended because the plugin was reloaded.";
     }
 
     public static String getNextGameMessage() {
+        // when a player tries to join a going game
+
         return ChatColor.GREEN + "A game is currently going, you will be put in the next game.";
     }
 }
