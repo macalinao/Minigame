a181a85a0c6560f36c6deece26fa441964463d30
Henry Ammermann <hintsss@gmail.com>
2014-02-17 16:55:09 -0800

    re-do countdown in a less stupid way

--- src/main/java/tk/hintss/minigame/util/CountdownTask.java
+++ src/main/java/tk/hintss/minigame/util/CountdownTask.java
@@ -1,56 +1,55 @@
 package tk.hintss.minigame.util;
 
-import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
-import tk.hintss.minigame.Minigame;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 public class CountdownTask extends BukkitRunnable {
-    private Player player;
+    // schedule this to run once a second
+
+    private Set<String> players;
     private int time;
     private String line;
 
-    public CountdownTask(Player player, int time, String line) {
-        this.player = player;
+    public CountdownTask(String player, int time, String line) {
+        this.players.add(player);
+        this.time = time;
+        this.line = line;
+    }
+
+    public CountdownTask(Set<String> players, int time, String line) {
+        this.players = players;
         this.time = time;
         this.line = line;
     }
 
     public void run() {
-        final int[] minutes = {15, 10, 5, 3, 2, 1};
-        final int[] seconds = {45, 30, 20, 15, 10, 5, 4, 3, 2, 1};
+        final List<Integer> minutes = Arrays.asList(new Integer[] {60, 45, 30, 15, 10, 5, 3, 2, 1});
+        final List<Integer> seconds = Arrays.asList(new Integer[] {45, 30, 20, 15, 10, 9, 8, 7, 6,  5, 4, 3, 2, 1});
 
-        if (time >= 60 * 20) {
-            for (int i = 0; i < minutes.length; i++) {
-                if (minutes[i] * 60 * 20 < time) {
-                    new CountdownTask(player, minutes[i] * 60 * 20, line).runTaskLater(Minigame.getInstance(), time - minutes[i] * 60 * 20);
-                    return;
-                } else if (minutes[i] * 60 * 20 == time) {
-                    player.sendMessage(String.format(line, Integer.valueOf(minutes[i]) + " minutes"));
-
-                    if (i < minutes.length - 1) {
-                        new CountdownTask(player, minutes[i] * 60 * 20, line).runTaskLater(Minigame.getInstance(), time - minutes[i] * 60 * 20);
-                    } else {
-                        new CountdownTask(player, seconds[0] * 20, line).runTaskLater(Minigame.getInstance(), time - seconds[0] * 20);
+        if (time >= 60 && time % 60 == 0) {
+            if (minutes.contains(time / 60)) {
+                for (String p : players) {
+                    if (Bukkit.getPlayer(p) != null) {
+                        Bukkit.getPlayer(p).sendMessage(String.format(line, String.valueOf(time / 60) + " minutes"));
                     }
-
-                    return;
                 }
             }
-        } else {
-            for (int i = 0; i < seconds.length; i++) {
-                if (seconds[i] * 20 < time) {
-                    new CountdownTask(player, minutes[i] * 60 * 20, line).runTaskLater(Minigame.getInstance(), time - minutes[i] * 60 * 20);
-                    return;
-                } else if (seconds[i] * 20 == time) {
-                    player.sendMessage(String.format(line, Integer.valueOf(seconds[i] + "seconds")));
-
-                    if (i < seconds.length - 1) {
-                        new CountdownTask(player, minutes[i] * 60 * 20, line).runTaskLater(Minigame.getInstance(), time - minutes[i] * 60 * 20);
+        } else if (time > 0) {
+            if (seconds.contains(time)) {
+                for (String p : players) {
+                    if (Bukkit.getPlayer(p) != null) {
+                        Bukkit.getPlayer(p).sendMessage(String.format(line, String.valueOf(time) + "seconds"));
                     }
-
-                    return;
                 }
             }
+        } else if (time <= 0) {
+            this.cancel();
         }
+
+        time--;
     }
 }
