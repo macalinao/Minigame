cbe1c087154933f2b769cb40b12d1b12ded489b5
Henry Ammermann <hintsss@gmail.com>
2014-05-01 00:41:04 -0700

    store health/food/saturation with playerorigin

--- src/main/java/tk/hintss/minigame/PlayerOrigin.java
+++ src/main/java/tk/hintss/minigame/PlayerOrigin.java
@@ -6,21 +6,36 @@ import org.bukkit.inventory.ItemStack;
 
 public class PlayerOrigin {
     private final String name;
+
     // isPlayer stores if the player joined the game as a player (and not a spectator)
     // if a game ends and this is true, they are auto-added to the next game
     private boolean isPlayer;
+
     private final Location originLoc;
+
+    private final double health;
+    private final int food;
+    private final float saturation;
+
     private final ItemStack[] inv;
     private final ItemStack[] armor;
+
     private final int exp;
 
     public PlayerOrigin(Player player, boolean isPlayer) {
         this.name = player.getName();
+
         this.isPlayer = isPlayer;
 
         this.originLoc = player.getLocation();
+
+        this.health = player.getHealth();
+        this.food = player.getFoodLevel();
+        this.saturation = player.getSaturation();
+
         this.inv = player.getInventory().getContents();
         this.armor = player.getInventory().getArmorContents();
+
         this.exp = player.getTotalExperience();
     }
 
@@ -28,8 +43,14 @@ public class PlayerOrigin {
         player.getInventory().clear();
 
         player.teleport(originLoc);
+
+        player.setHealth(health);
+        player.setFoodLevel(food);
+        player.setSaturation(saturation);
+
         player.getInventory().setContents(inv);
         player.getInventory().setArmorContents(armor);
+
         player.setTotalExperience(exp);
     }
 
